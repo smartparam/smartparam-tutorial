df48d21c74c2295ae4d55d6865d0efc98469f2d0
Adam Dubiel <dubiel.adam@gmail.com>
2014-02-25 20:08:59 +0100

    smartparam version 1.1.1 compatibility
    Conflicts:
    	src/main/java/org/smartparam/tutorial/config/RootContext.java

--- src/test/java/org/smartparam/tutorial/DiscountCalculatorTest.java
+++ src/test/java/org/smartparam/tutorial/DiscountCalculatorTest.java
@@ -26,8 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
-
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  *
